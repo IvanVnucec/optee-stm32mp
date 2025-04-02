8b9b63fd7814220efabf9e6acc810e4761d33898
Ivan Vnucec <vnucec.ivan@gmail.com>
2025-04-02 20:00:04 +0200

    add platform tag

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@
 
 set -x -e
 
+PLATFORM=stm32mp1-157_SOMIC
+
 rm -rf build
-make -j8 DEBUG=1 CFG_TEE_CORE_LOG_LEVEL=4 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=stm32mp1-157_SOMIC O=build all
+make -j8 DEBUG=1 CFG_TEE_CORE_LOG_LEVEL=4 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=$PLATFORM O=build all
