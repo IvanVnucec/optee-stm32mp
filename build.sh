ddc1e8ba86db4f932e143ac5c2b515848032fc72
Ivan Vnucec <vnucec.ivan@gmail.com>
2025-04-02 18:45:12 +0200

    add debug info

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 set -x -e
 
 rm -rf build
-make -j8 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=stm32mp1-157_SOMIC O=build all
+make -j8 DEBUG=1 CFG_TEE_CORE_LOG_LEVEL=4 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=stm32mp1-157_SOMIC O=build all
