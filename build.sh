837b8c53233dd65083eb9cf3d32eade553ebbf7f
Ivan Vnucec <vnucec.ivan@gmail.com>
2025-04-08 11:55:35 +0200

    remove debug flags

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -x -e
 PLATFORM=stm32mp1-157_SOMIC
 
 rm -rf build
-make -j8 DEBUG=1 CFG_TEE_CORE_LOG_LEVEL=4 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=$PLATFORM O=build all
+make -j8 CROSS_COMPILE=arm-linux-gnueabihf- PLATFORM=$PLATFORM O=build all
+# DEBUG=1 CFG_TEE_CORE_LOG_LEVEL=3
